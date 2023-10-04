b85b4832dca85b5531730e27c5af97046d26821c
ymfm <2827863123cnbb@gmail.com>
2023-10-05 06:16:29 +1200

    search test 1

--- backend/src/app.js
+++ backend/src/app.js
@@ -16,25 +16,12 @@ mongoose.connect('mongodb+srv://Duhang:xjd7604@cise.jgh9sxb.mongodb.net/?retryWr
     console.error('Failed to connect to MongoDB', err);
   });
 
-// Mongoose model
-const User = mongoose.model('User', { Name: String, id: String });
-
 app.use(cors());  // Enable CORS for all routes
 app.use(express.json());  // Middleware to parse JSON requests
 
-app.post('/api/findUser', async (req, res) => {
-  console.log("Received a request at /api/findUser")
-  try {
-    const user = await User.findOne({ Name: req.body.Name });
-    if (user) {
-      res.json({ id: user.id });
-    } else {
-      res.status(404).json({ message: 'User not found!' });
-    }
-  } catch (error) {
-    res.status(500).json({ message: 'Internal server error' });
-  }
-});
+// 导入查询数据库的路由
+const dbRoutes = require('./search.tsx');
+app.use('/api', dbRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,47 +2,63 @@
 import React, { useState } from "react";
 import { CiSearch } from 'react-icons/ci';
 
+
 import AppBar from "./components/AppBar";
 const HomePage: React.FC = () => {
+  const [searchQuery, setSearchQuery] = useState('');
+  const [searchResult, setSearchResult] = useState('');
+
+  const handleSearch = async () => {
+    try {
+      const response = await fetch('http://localhost:5000/api/query', {
+        method: 'GET', // 使用 GET 请求触发查询操作
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        setSearchResult(`User ID: ${data.id}`);
+      } else {
+        setSearchResult('User not found');
+      }
+    } catch (error) {
+      console.error('There was an error:', error);
+    }
+  };
 
   return (
-    
     <div>
       <AppBar />
-
       <div className="container mx-auto px-4">
-  <h1>Explore the world’s knowledge, cultures, and ideas</h1>
-  <div className="search-bar">
-    <input type="text" placeholder="Search..." />
-    <span className="search-icon"><CiSearch/></span>
-  </div>
-  <style jsx>{`
-    h1 {
-      font-family: "Times New Roman", Times, serif;
-    }
-    .search-bar {
-      margin-top: 20px;
-      display: flex;
-      align-items: center;
-      border: 1px solid #ccc;
-      padding: 8px;
-    }
-    .search-bar input {
-      flex: 1;
-      border: none;
-      outline: none;
-      font-size: 16px;
-    }
-    .search-icon {
-      margin-left: 8px;
-      font-size: 24px;
-      cursor: pointer;
-    }
-  `}</style>
-</div>
-
-
-
+        <h1>Explore the world’s knowledge, cultures, and ideas</h1>
+        <div className="search-bar">
+          <input type="text" placeholder="Search..." value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}/>
+          <span className="search-icon" onClick={handleSearch}><CiSearch/></span> {}
+        </div>
+        <p>{searchResult}</p>
+      </div>
+      <style jsx>{`
+        h1 {
+          font-family: "Times New Roman", Times, serif;
+        }
+        .search-bar {
+          margin-top: 20px;
+          display: flex;
+          align-items: center;
+          border: 1px solid #ccc;
+          padding: 8px;
+        }
+        .search-bar input {
+          flex: 1;
+          border: none;
+          outline: none;
+          font-size: 16px;
+        }
+        .search-icon {
+          margin-left: 8px;
+          font-size: 24px;
+          cursor: pointer;
+        }
+      `}</style>
     </div>
   );
 };
